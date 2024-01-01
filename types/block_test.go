83dc1255029340062454cea3d5df0e9b084434b4
Ananya <ananyakaushik48@gmail.com>
2024-01-01 23:08:28 +0530

    Added the block test

--- types/block_test.go
+++ types/block_test.go
@@ -1,7 +1,14 @@
 package types
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/ananyakaushik48/ed25519/util"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestHashBlock(t *testing.T) {
-	
+	block := util.RandomBlock()
+	hash := HashBlock(block)
+	assert.Equal(t, 32, len(hash))
 }
